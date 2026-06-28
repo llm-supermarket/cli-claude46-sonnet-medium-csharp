c611a48d2671b1b1223d654b72474b10fe8c9e8a
yetanotherchris <54912+yetanotherchris@users.noreply.github.com>
2026-06-28 10:32:50 +0000

    chore: update package manifests to v0.0.1

--- Formula/cli-claude46-sonnet-medium-csharp.rb
+++ Formula/cli-claude46-sonnet-medium-csharp.rb
@@ -6,20 +6,20 @@ class CliClaude46SonnetMediumCsharp < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/llm-supermarket/cli-claude46-sonnet-medium-csharp/releases/download/v0.0.1/cli-claude46-sonnet-medium-csharp-darwin-arm64.tar.gz"
-      sha256 "placeholder"
+      sha256 "5ee1a466bbf36f93c8a3a226a0f96ae35f3f0991bddf43e96f7f6a32192f4cd2"
     else
       url "https://github.com/llm-supermarket/cli-claude46-sonnet-medium-csharp/releases/download/v0.0.1/cli-claude46-sonnet-medium-csharp-darwin-amd64.tar.gz"
-      sha256 "placeholder"
+      sha256 "79638dde0dcc63359e5e66bcb17607797e11fe2db4e9452cdf428498d5fa9cee"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/llm-supermarket/cli-claude46-sonnet-medium-csharp/releases/download/v0.0.1/cli-claude46-sonnet-medium-csharp-linux-arm64.tar.gz"
-      sha256 "placeholder"
+      sha256 "eaa216849aea3cc018047e07063614503432ee5e0aec3d0fbd2427adf143f804"
     else
       url "https://github.com/llm-supermarket/cli-claude46-sonnet-medium-csharp/releases/download/v0.0.1/cli-claude46-sonnet-medium-csharp-linux-amd64.tar.gz"
-      sha256 "placeholder"
+      sha256 "ea68a4f1d17c548558cf492e0eabcd78b421460c2ca9c8eb8b664de08dcb9344"
     end
   end
 
